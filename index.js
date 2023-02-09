7da0951a95d7217c1d9b99e829d9b86e964db62d
Alex <alexpinheiropf@hotmail.com>
2023-02-09 14:04:15 -0300

    Limpando e comentando o código

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(express.json())
 // Configura a porta padrão ou a porta do Heroku teste
 server.listen(port)
 
-// Rota responsável em retornar o parâmetro 
+// Rota responsável em retornar o parâmetro das opções
 server.get('/opcao/:serieId', async (req, res) => {
 
     // Recebe o parâmetro do Ticker da Opção
@@ -36,21 +36,20 @@ server.get('/opcao/:serieId', async (req, res) => {
     }
 })
 
-server.get('/teste', async (req, res) => {
-
-    return res.send('teste')
-})
-
-// Rota responsável em retornar o parâmetro 
+// Rota responsável em retornar o parâmetro do tesouro direto
 server.get('/tesouro/:ticker', async (req, res) => {
 
     // Recebe o parâmetro do Ticker da Opção
     const { ticker } = req.params
+
+    // Pega a data atual para comparar os valores
     const hoje = new Date()
     const dia = hoje.getDate().toString().padStart(2,'0')
     const mes = String(hoje.getMonth() + 1).padStart(2,'0')
     const ano = hoje.getFullYear().toString().slice(2)
     const dataAtual = `${dia}/${mes}/${ano}`
+
+    // Seta o preço como 0
     let price = 0
 
     try {
@@ -58,8 +57,10 @@ server.get('/tesouro/:ticker', async (req, res) => {
         // Inclui o parâmetro do Ticker do Tesouro e retorna todos os preços
         const {data} = await api.get(`category/bondprice?ticker=${ticker}`)
 
+        // Realiza um for e paga o último preço do ativo
         data.forEach(el => {
           if(dataAtual === el.date) {
+            // Valida se possui o ultimo preço atual senão pega o ultimo valor guardado
             price = el.buyprice ?? el.sellprice
           }
         });
